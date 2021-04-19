b619421492b527d6d960c93da6191092c5e629d2
greywolf37 <rubin3737.rt@gmail.com>
2021-04-19 03:52:27 -0400

    forward_sw: Added intermediate array deletion (TESTED)

--- src/host.cpp
+++ src/host.cpp
@@ -33,48 +33,6 @@ int main(int argc, char** argv){
     std::cout << "Output" << std::endl;
     std::cout << output << std::endl;
 
-
-    // torch::Tensor tensor = torch::rand({1, 1, 3, 3});
-    // std::cout << "tensor" << std::endl;
-    // std::cout << tensor << std::endl;
-
-    // torch::Tensor output = forward_sw(tensor, tensor);
-    // std::cout << "Output" << std::endl;
-    // std::cout << output << std::endl;
-
-    // float * output_arr = tensor2arr_4d(output);
-    // std::cout << "Output Array" << std::endl;
-    // print_tensor(output_arr, output.size(0),output.size(1),output.size(2),output.size(3));
-
-    // torch::Tensor output2= arr2tensor_4d(output_arr, output.size(0),output.size(1),output.size(2),output.size(3));
-    // std::cout << "Output Tensor 2" << std::endl;
-    // std::cout << output2 << std::endl;
-
-    // return 0;
-
-
-
-
-    // int out_channels=2;
-    // int in_channels=2;
-    // int in_height=3;
-    // int in_width=3;
-
-    // int out_height;
-    // int out_width;
-
-    // float input_tensor[out_channels*in_channels*in_height*in_width];
-    // std::cout<<"Input:";
-    // init_tensor(input_tensor, out_channels, in_channels, in_height, in_width);
-    // print_tensor(input_tensor, out_channels, in_channels, in_height, in_width);
-
-    // float *out_tensor = weight2col(input_tensor, out_channels, in_channels, in_height, in_width, &out_height, &out_width);
-
-    // std::cout<<"Output:";
-    // print_tensor(out_tensor, 1, 1, out_height, out_width);
-    
-    // // Remember to delete memory
-    // delete[] out_tensor;
 }
 
 
@@ -111,6 +69,8 @@ torch::Tensor forward_sw(torch::Tensor input, torch::Tensor weights){
     // Converting array to tensor
     torch::Tensor output = arr2tensor_4d(out_array, batches, out_channels, out_height, out_width);
     
+    // Delete intermediate arrays
+    delete[] in_array_col, kernel_array_col, output_col;
 
     return output;
 }
